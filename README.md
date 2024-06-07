ccc660e67f24daeb99eea22224b478fcd3aa30bd
Bahaatbb <bahaatbb@gmail.com>
2024-06-07 11:04:31 +0300

    Add a more Optimized Version

--- README.md
+++ README.md
@@ -11,13 +11,28 @@ This repository contains a simple implementation of matrix multiplication using 
 ### Compiliation
 To compile the code, use the following command:
 ```bash
- gcc -o gemm gemm.c  -O3 -ffast-math -fopenmp
+ gcc -o gemm gemm.c -O3 -ffast-math -fopenmp -march=native
 ```
 
-## Performance progress
-This benchmark was run on an M2 Pro processor.
-![Image show the flops progression](./flops.png)
+## Matrix Multiplication Benchmark on M2 Pro Processor
+This benchmark compares the performance of four different matrix multiplication implementations on an M2 Pro processor.
+Implementations:
+1. Optimized Neon Parallel BLOCKED
+2. Standard Neon Parallel BLOCKED
+3. Normal Parallel NEON
+4. Normal Parallel matmul
+
+### Results:
+1. N = 1024, BLOCK_SIZE = 16
+  - Optimized: 87.17 GFLOP/S 
+  - Standard: 69.49 GFLOP/S 
+  - Normal NEON: 76.44 GFLOP/S 
+  - Normal matmul: 4.85 GFLOP/S s
+2. N = 8192, BLOCK_SIZE = 16
+  - Optimized: 122.27 GFLOP/S ms
+  - Standard: 72.04 GFLOP/S  ms
+  - Normal NEON: 60.23 GFLOP/S  ms
+  - Normal matmul: Not applicable
 
 > [!NOTE] 
->
-> The code demonstrates the effectiveness of vectorized operations and the utilization of blocked matrix multiplication for enhanced performance.
+> Our old best performance was ~76 GFLOP/S, and we have now achieved a  significant improvement of 122.27 GFLOP/S with our optimized implementation.
--- gemm.c
+++ gemm.c
@@ -1,20 +1,13 @@
 #include <arm_neon.h>
 #include <math.h>
+#include <omp.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <omp.h>
 
-#define N 8192
-#define BLOCK_SIZE 32
-
-float A[N * N] __attribute__((aligned(64)));
-float B[N * N] __attribute__((aligned(64)));
-float BT[N * N] __attribute__((aligned(64)));
-float C[N * N] __attribute__((aligned(64)));
-float val[N * N] __attribute__((aligned(64)));
-float val2[N * N] __attribute__((aligned(64)));
+#define N 2048
+#define BLOCK_SIZE 16
 
 int check_equality(float *matrix, float *matrix2) {
   float diff = 0;
@@ -39,8 +32,24 @@ uint64_t nanos() {
   clock_gettime(CLOCK_MONOTONIC_RAW, &start);
   return (uint64_t)start.tv_sec * 1000000000 + (uint64_t)start.tv_nsec;
 }
+float *A;
+float *B;
+float *BT;
+float *C;
+float *val;
+float *val2;
+float *val3;
 
 int main() {
+  // Dynamic Creation with memory alignment 
+  posix_memalign((void **)&A, 64, N * N * sizeof(float));
+  posix_memalign((void **)&B, 64, N * N * sizeof(float));
+  posix_memalign((void **)&BT, 64, N * N * sizeof(float));
+  posix_memalign((void **)&C, 64, N * N * sizeof(float));
+  posix_memalign((void **)&val, 64, N * N * sizeof(float));
+  posix_memalign((void **)&val2, 64, N * N * sizeof(float));
+  posix_memalign((void **)&val3, 64, N * N * sizeof(float));
+
   for (int i = 0; i < N * N; i++) {
     A[i] = (float)(rand() % 100);
     B[i] = (float)(rand() % 100);
@@ -54,6 +63,45 @@ int main() {
 
   uint64_t start = nanos();
   int i, j, k, kk, jj;
+#pragma omp parallel for private(i, j, k, kk, jj) shared(A, BT, val)
+  for (i = 0; i < N; i += BLOCK_SIZE) {
+    for (j = 0; j < N; j += BLOCK_SIZE) {
+      for (kk = i; kk < i + BLOCK_SIZE && kk < N; ++kk) {
+        for (jj = j; jj < j + BLOCK_SIZE && jj < N; ++jj) {
+          float32x4_t sum0 = vdupq_n_f32(0.0);
+          float32x4_t sum1 = vdupq_n_f32(0.0);
+          float32x4_t sum2 = vdupq_n_f32(0.0);
+          float32x4_t sum3 = vdupq_n_f32(0.0);
+
+          __builtin_prefetch(&A[kk * N], 0, 3);
+          __builtin_prefetch(&BT[jj * N], 0, 3);
+
+          for (k = 0; k < N; k += 16) {
+            float32x4x4_t va = vld4q_f32(&A[kk * N + k]);
+            float32x4x4_t vb = vld4q_f32(&BT[jj * N + k]);
+
+            sum0 = vmlaq_f32(sum0, va.val[0], vb.val[0]);
+            sum1 = vmlaq_f32(sum1, va.val[1], vb.val[1]);
+            sum2 = vmlaq_f32(sum2, va.val[2], vb.val[2]);
+            sum3 = vmlaq_f32(sum3, va.val[3], vb.val[3]);
+          }
+
+          float32x4_t sum =
+              vaddq_f32(vaddq_f32(sum0, sum1), vaddq_f32(sum2, sum3));
+          val3[jj * N + kk] = vaddvq_f32(sum);
+        }
+      }
+    }
+  }
+  double gflop = (2.0 * N * N * N) * 1e-9;
+  uint64_t end = nanos();
+  double s = (end - start) * 1e-9;
+  printf("Optimized Neon Parallel BLOCKED Matrix Multiplication (N = %d, "
+         "BLOCK_SIZE = %d) achieves %.2f GFLOP/S in %.2f ms\n",
+         N, BLOCK_SIZE, gflop / s, s * 1e3);
+
+  start = nanos();
+  i = 0, j = 0, k = 0, kk = 0, jj = 0;
 #pragma omp parallel for private(i, j, k, kk, jj) shared(A, BT, val)
   for (i = 0; i < N; i += BLOCK_SIZE) {
     for (j = 0; j < N; j += BLOCK_SIZE) {
@@ -74,10 +122,10 @@ int main() {
       }
     }
   }
-  uint64_t end = nanos();
-  double gflop = (2.0 * N * N * N) * 1e-9;
-  double s = (end - start) * 1e-9;
-  printf("Neon Parallel BLOCKED matmul gets: %f GFLOP/S -- %.2f ms\n", gflop / s, s * 1e3);
+  end = nanos();
+  s = (end - start) * 1e-9;
+  printf("Standard Neon Parallel BLOCKED Matrix Multiplication (N = %d, BLOCK_SIZE = %d) takes %.2f ms, achieving a performance of %.2f GFLOP/S\n",
+         N, BLOCK_SIZE, s * 1e3, gflop / s);
 
   start = nanos();
 #pragma omp parallel for collapse(2)
@@ -98,8 +146,8 @@ int main() {
   }
   end = nanos();
   s = (end - start) * 1e-9;
-  printf("Normal Parallel NEON matmul gets: %f GFLOP/S -- %.2f ms\n", gflop / s,
-         s * 1e3);
+ printf("Normal Parallel NEON Matrix Multiplication (N = %d) takes %.2f ms, achieving a performance of %.2f GFLOP/S\n",
+         N, s * 1e3, gflop / s);
 
   start = nanos();
 #pragma omp parallel for collapse(2)
@@ -114,8 +162,17 @@ int main() {
   }
   end = nanos();
   s = (end - start) * 1e-9;
-  printf("Normal Parallel matmul gets: %f GFLOP/S -- %.2f ms\n", gflop / s, s * 1e3);
+  printf("Normal Parallel matmul gets: %f GFLOP/S -- %.2f ms\n", gflop / s,
+         s * 1e3);
 
-  printf("\nThe difference is : %d", check_equality(C, val2));
+  printf("\nThe difference is : %d", check_equality(C, val3));
+
+  free(A);
+  free(B);
+  free(BT);
+  free(C);
+  free(val);
+  free(val2);
+  free(val3);
   return 0;
 }
